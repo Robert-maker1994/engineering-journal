1221fc786897122aeb1e51a061db37b339be47a4
Levono Hummus <Robertpaullawson@gmail.com>
2024-11-01 21:25:42 +0100

    make fancy string

---
+++ leet_code_katas/src/katas/make_fancy_string.rs
@@ -0,0 +1,50 @@
+struct Solution;
+
+impl Solution {
+    pub fn make_fancy_string(s: String) -> String {
+        let mut max = 1; // Start with 1 since the first character is always added
+        let by = s.as_bytes();
+        let mut result = String::new();
+
+        result.push(by[0] as char); // Add the first character to the result
+
+        for i in 1..by.len() {
+            if by[i] == by[i - 1] {
+                max += 1;
+            } else {
+                max = 1;
+            }
+
+            if max < 3 {
+                result.push(by[i] as char);
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn make_fancy_string_1() {
+        assert_eq!(Solution::make_fancy_string("leeetcode".to_string()), "leetcode".to_string());
+    }
+
+    #[test]
+    fn make_fancy_string_2() {
+        assert_eq!(Solution::make_fancy_string("aaabaaaa".to_string()), "aabaa".to_string());
+    }
+
+    #[test]
+    fn make_fancy_string_3() {
+        assert_eq!(Solution::make_fancy_string("aab".to_string()), "aab".to_string());
+    }
+}
+
+fn main() {
+    let result = Solution::make_fancy_string("leeetcode".to_string());
+    println!("Result: {}", result); // Output should be "leetcode"
+}
