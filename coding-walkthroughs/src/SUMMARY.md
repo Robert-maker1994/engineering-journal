5572eee352f8a40f916163a8de81fea37830736e
Robert Lawson <Robertpaullawson@gmail.com>
2025-01-20 15:24:16 +0100

    longest common prefix

--- coding-walkthroughs/src/SUMMARY.md
+++ coding-walkthroughs/src/SUMMARY.md
@@ -3,5 +3,7 @@
     - [Defuse Bomb](./arrays/defuse_bomb.md)
     - [Max Kelements](./arrays/max_kelements.md)
     - [Maxium Subarray Sub](./arrays/maxium_subarray_sub.md)
+  - [Hash Tables](./hash-tables/README.md)
+    - [Majority Element](./hash-tables/majority-element.md)
   - [Queues](./queues/README.md)
     - [Shortest Subarray](./queues/shortest_subarray.md)
--- playground.ts
+++ playground.ts
@@ -1,71 +1,13 @@
-
-// 14 miliseconed 
-// function majorityElement(nums: number[]) {
-//    const m = nums.length / 2;
-
-//    const map = new Map<number, number>();
-   
-//     // Add all the variable to the map
-//    for (const n of nums) {
-//         if(!map.has(n)) {
-//             map.set(n, 0)
-//         }
-//         map.set(n, map.get(n)! + 1)
-//    }
-
-//    let highest = 0;
-//    for (const [key, val] of map.entries()) {
-//     if(val > m) {
-
-//         if (val > highest) {
-//             highest = val;
-//             return key;
-//         }
-//     }
-//    }
-// };
-
-// 10ms
-// function majorityElement(nums: number[]) {
-//     const m = nums.length / 2;
- 
-//     const countArray: [number, number][] = [];
-
-//     // Add all the variables to the countArray
-//     for (const n of nums) {
-//         const index = countArray.findIndex(([num]) => num === n);
-//         if (index === -1) {
-//             countArray.push([n, 1]);
-//         } else {
-//             countArray[index][1]++;
-//         }
-//     }
- 
-//     let highest = 0;
-//     for (const [key, val] of countArray) {
-//      if(val > m) {
- 
-//          if (val > highest) {
-//              highest = val;
-//              return key;
-//          }
-//      }
-//     }
-//  };
-
-function majorityElement(nums: number[]) {
-    let candidate;
-    let count = 0;
+function longestCommonPrefix(strs: string[]): string {
+    let prefix =  strs[0]; 
     
-    for (const num of nums) {
-        if (count === 0) {
-            console.log("num", count)
-            candidate = num;
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return ""; // No common prefix
         }
-        count += (num === candidate) ? 1 : -1
-        console.log("num & candidate",{ candidate, num, count})
     }
-    
-    return candidate;
+    return prefix
 };
 
+longestCommonPrefix(["flower", "flow", "flight"])
